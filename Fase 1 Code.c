dca42294d7a212c2d90131ecc8817831b82e70c4
Layfyy <lucasalves@sapo.pt>
2024-03-12 22:56:30 +0000

    commit

--- Fase 1 Code.c
+++ Fase 1 Code.c
@@ -17,6 +17,7 @@
     filename = Nome do ficheiro que contem a matriz
     file = Ficheiro que contem a matriz
     line = Linha do ficheiro
+    token = Valor do numero, ele considera o valor até ao ";"
     maxRow = Numero maximo de linhas
     maxColumn = Numero maximo de colunas
     choice = Escolha do utilizador
@@ -26,6 +27,8 @@
     rowIndex = Indice da linha a ser removida
     columnIndex = Indice da coluna a ser removida
     i = Variavel de controle para loops
+    j = Variavel de controle para loops
+    reasing = Node temporario para percorrer a linked list
 */
 
 //Constituição do node da linked list
@@ -101,7 +104,6 @@ void printListInOrder(Node* node) {
     //Basicamente, chama continuamente a função para o proximo node até que o node seja NULL
     //Quando o node for NULL, a função retornará para o node anterior e imprimirá o valor do node
     printListInOrder(node->next);
-
     printf("Data: %d, Column: %d, Row: %d\n", node->data, node->column, node->row);
 }
 
@@ -167,8 +169,9 @@ void changeNodeValue(Node* head, int row, int column, int newValue) {
 
 
 //Funçao para adicionar uma linha à matriz
-void addRow(int maxColumn, int maxRow) {
+void addRow(int maxColumn, int maxRow, int rowIndex) {
     int newValueForNewLine;
+    Node *temp = head;
     //Perguntar ao usuario quais valores ele deseja adicionar à nova linha e adicionar os valores à linked list
     for  (int i = 0; i <= maxColumn; i++) {
         printf("Indique o %dº valor da nova linha: ", (i+1));
@@ -180,16 +183,28 @@ void addRow(int maxColumn, int maxRow) {
         //Atribuir os valores ao novo node
         newNode->data = newValueForNewLine; //Numero 
         newNode->column = i; //Coluna
-        newNode->row = maxRow + 1; //Linha
+        newNode->row = rowIndex; //Linha
         newNode->next = head; //Apontar para o proximo node
         head = newNode; //Atualizar a head para apontar para o novo node
     }
+
+    //Atualizar os índices das linhas
+    while (temp != NULL) {
+        if (temp->row >= rowIndex) {
+            temp->row = temp->row + 1;
+        }
+        temp = temp->next;
+    }
+
+    //Atualizar o número máximo de linhas
+    maxRow++;
 }
 
 
 //Função para adicionar uma coluna à matriz
-void addColumn(int maxColumn, int maxRow) {
+void addColumn(int maxColumn, int maxRow, int columnIndex) {
     int newValueForNewColumn;
+    Node *temp = head;
     //Perguntar ao usuario quais valores ele deseja adicionar à nova coluna e adicionar os valores à linked list
     for  (int i = 0; i <= maxRow; i++) {
         printf("Indique o %dº valor da nova coluna: ", (i+1));
@@ -200,11 +215,22 @@ void addColumn(int maxColumn, int maxRow) {
 
         //Atribuir os valores ao novo node
         newNode->data = newValueForNewColumn; //Numero 
-        newNode->column = maxColumn + 1; //Coluna
+        newNode->column = columnIndex; //Coluna
         newNode->row = i; //Linha
         newNode->next = head; //Apontar para o proximo node
         head = newNode; //Atualizar a head para apontar para o novo node
     }
+
+    //Atualizar os índices das colunas
+    while (temp != NULL) {
+        if (temp->column >= columnIndex) {
+            temp->column = temp->column + 1;
+        }
+        temp = temp->next;
+    }
+
+    //Atualizar o número máximo de colunas
+    maxColumn++;
 }
 
 
@@ -236,12 +262,14 @@ void removeRow(int rowIndex) {
         }
     }
 
+    Node* reasing = head;
+
     //Atualizar os índices das linhas
-    while (temp != NULL) {
-        if (temp->row > rowIndex) {
-            temp->row = temp->row - 1;
+    while (reasing != NULL) {
+        if (reasing->row >= rowIndex) {
+            reasing->row = reasing->row - 1;
         }
-        temp = temp->next;
+        reasing = reasing->next;
     }
 
     //Atualizar o número máximo de linhas
@@ -268,7 +296,7 @@ void removeColumn(int columnIndex) {
 
             Node* nodeToRemove = temp;
             temp = temp->next;
-
+            
             free(nodeToRemove);
         } else {
             //Se o node atual não deve ser removido
@@ -277,12 +305,14 @@ void removeColumn(int columnIndex) {
         }
     }
 
+    Node* reasing = head;
+
     //Atualizar os índices das colunas
-    while (temp != NULL) {
-        if (temp->column > columnIndex) {
-            temp->column = temp->column - 1;
+    while (reasing != NULL) {
+        if (reasing->column >= columnIndex) {
+            reasing->column = reasing->column - 1;
         }
-        temp = temp->next;
+        reasing = reasing->next;
     }
 
     //Atualizar o número máximo de colunas
@@ -296,6 +326,9 @@ void chooseWhatToDo() {
     int row;
     int column;
     int newValue;
+
+    //printf("\n");
+    //printListInOrder(head);
 
     //Imprimir a matriz sempre que o menu for exibido
     printMatrixFromList();
@@ -340,11 +373,27 @@ void chooseWhatToDo() {
             break;
         case 2:
             //Adicionar uma linha
-            addRow(maxColumn, maxRow);
+            printf("Escolha a linha que deseja adicionar: ");
+            scanf("%d", &row);
+
+            if (row > maxRow + 1 || row < 1)
+            {
+                printf("Linha inválida.\n");
+                break;
+            }
+            addRow(maxColumn, maxRow, row - 1);
             break;
         case 3:
             //Adicionar uma coluna
-            addColumn(maxColumn, maxRow);
+            printf("Escolha a coluna que deseja adicionar: ");
+            scanf("%d", &column);
+
+            if (column > maxColumn + 1 || column < 1)
+            {
+                printf("Coluna inválida.\n");
+                break;
+            }
+            addColumn(maxColumn, maxRow, column - 1);
             break;
         case 4:
             //Remover uma linha
@@ -359,7 +408,7 @@ void chooseWhatToDo() {
             break;
         case 5:
             //Remover uma coluna
-            printf("Escolha a linha que deseja remover: ");
+            printf("Escolha a coluna que deseja remover: ");
             scanf("%d", &column);
 
             if (column - 1 > maxColumn || column < 1){
@@ -387,7 +436,6 @@ void chooseWhatToDo() {
 //Função main
 int main() {
     loadMatrixToList("matriz.txt");
-    //printListInOrder(head);
     chooseWhatToDo();
     return 0;
 }
